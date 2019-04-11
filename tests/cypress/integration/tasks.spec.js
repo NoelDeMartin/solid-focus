0df1c53172e827f1404882a5c75bbc4298b21ec2
Noel De Martin <noeldemartin@gmail.com>
2019-04-11 20:38:31 +0200

    Implement tests for workspaces and lists creation

--- tests/cypress/integration/tasks.spec.js
+++ tests/cypress/integration/tasks.spec.js
@@ -1,18 +1,48 @@
+import Faker from 'faker';
+
 describe('Tasks', () => {
 
     beforeEach(() => {
         cy.login();
-        cy.addWorkspace('Tasks');
     });
 
-    it('Creates task', () => {
+    it('Creates a workspace', () => {
+        const name = Faker.lorem.sentence();
+
         cy.visit('/');
 
-        cy.get('input').type('Watch Avengers: Endgame');
+        cy.get('[title="Create new workspace"]').click();
 
-        cy.get('[title="Create new task"]').click();
+        cy.get('input').type(name).type('{enter}');
 
-        cy.contains('Watch Avengers: Endgame');
+        cy.contains(name);
+        cy.contains('Inbox');
+    });
+
+    it('Creates a list', () => {
+        const name = Faker.lorem.sentence();
+
+        cy.addWorkspace(Faker.lorem.sentence());
+
+        cy.contains('a', 'Add List').click();
+
+        cy.get('.v-dialog').within(() => {
+            cy.get('input').type(name).type('{enter}');
+        });
+
+        cy.contains(name);
+    });
+
+    it('Creates a task', () => {
+        const name = Faker.lorem.sentence();
+
+        cy.addWorkspace(Faker.lorem.sentence());
+
+        cy.visit('/');
+
+        cy.get('input').type(name).type('{enter}');
+
+        cy.contains(name);
     });
 
 });
