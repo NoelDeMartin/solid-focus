c09c1ca929f72f30b9fbf4cd34db4c31c8e1852a
Noel De Martin <noel@noeldemartin.com>
2022-02-27 08:22:24 +0100

    deploy

---
+++ precache-manifest.f32e0d7fbf7d39f551badb599ae23a5d.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = [
+  {
+    "revision": "210a7c781f5a354a0e4985656ab456d9",
+    "url": "/solid-focus/fonts/roboto-latin-300italic.210a7c78.woff"
+  },
+  {
+    "revision": "1b6aaba092cc0a1422bb9617b54c9f95",
+    "url": "/solid-focus/logo.svg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/solid-focus/robots.txt"
+  },
+  {
+    "revision": "bafb105baeb22d965c70fe52ba6b49d9",
+    "url": "/solid-focus/fonts/roboto-latin-400.bafb105b.woff"
+  },
+  {
+    "revision": "817ea2cf37875fb12bcc",
+    "url": "/solid-focus/js/chunk-2d0b3e1d.87116961.js"
+  },
+  {
+    "revision": "00d37a0148348e7b9ba9",
+    "url": "/solid-focus/js/chunk-2d0db283.c2cd132a.js"
+  },
+  {
+    "revision": "338c8f0f53bc4379580e",
+    "url": "/solid-focus/js/chunk-2d0df226.d6a8824a.js"
+  },
+  {
+    "revision": "df51a066d0db455a9e98",
+    "url": "/solid-focus/js/chunk-795096ba.505a5bdc.js"
+  },
+  {
+    "revision": "154bc21a68bd2bf21a35",
+    "url": "/solid-focus/js/chunk-2d0c7a58.e8dabac1.js"
+  },
+  {
+    "revision": "773cb6aca9446d02c7d0",
+    "url": "/solid-focus/js/chunk-vendors.9ca30d13.js"
+  },
+  {
+    "revision": "de8b7431b74642e830af4d4f4b513ec9",
+    "url": "/solid-focus/fonts/roboto-latin-500.de8b7431.woff"
+  },
+  {
+    "revision": "987b84570ea69ee660455b8d5e91f5f1",
+    "url": "/solid-focus/fonts/roboto-latin-100.987b8457.woff2"
+  },
+  {
+    "revision": "5d4aeb4e5f5ef754e307d7ffaef688bd",
+    "url": "/solid-focus/fonts/roboto-latin-400.5d4aeb4e.woff2"
+  },
+  {
+    "revision": "285467176f7fe6bb6a9c6873b3dad2cc",
+    "url": "/solid-focus/fonts/roboto-latin-500.28546717.woff2"
+  },
+  {
+    "revision": "037d830416495def72b7881024c14b7b",
+    "url": "/solid-focus/fonts/roboto-latin-700.037d8304.woff2"
+  },
+  {
+    "revision": "19b7a0adfdd4f808b53af7e2ce2ad4e5",
+    "url": "/solid-focus/fonts/roboto-latin-900.19b7a0ad.woff2"
+  },
+  {
+    "revision": "6232f43d15b0e7a0bf0fe82e295bdd06",
+    "url": "/solid-focus/fonts/roboto-latin-100italic.6232f43d.woff2"
+  },
+  {
+    "revision": "d704bb3d579b7d5e40880c75705c8a71",
+    "url": "/solid-focus/fonts/roboto-latin-100italic.d704bb3d.woff"
+  },
+  {
+    "revision": "e9dbbe8a693dd275c16d32feb101f1c1",
+    "url": "/solid-focus/fonts/roboto-latin-100.e9dbbe8a.woff"
+  },
+  {
+    "revision": "a1471d1d6431c893582a5f6a250db3f9",
+    "url": "/solid-focus/fonts/roboto-latin-300.a1471d1d.woff"
+  },
+  {
+    "revision": "4b4fba4cb9ed9afbcd64",
+    "url": "/solid-focus/js/app.7d8f78ce.js"
+  },
+  {
+    "revision": "d69924b98acd849cdeba9fbff3f88ea6",
+    "url": "/solid-focus/fonts/roboto-latin-300italic.d69924b9.woff2"
+  },
+  {
+    "revision": "f3794e265d67eecacffc",
+    "url": "/solid-focus/js/chunk-228c3714.6f3e5267.js"
+  },
+  {
+    "revision": "d8bcbe724fd6f4ba44d0ee6a2675890f",
+    "url": "/solid-focus/fonts/roboto-latin-400italic.d8bcbe72.woff2"
+  },
+  {
+    "revision": "9680d5a0c32d2fd084e07bbc4c8b2923",
+    "url": "/solid-focus/fonts/roboto-latin-400italic.9680d5a0.woff"
+  },
+  {
+    "revision": "55536c8e9e9a532651e3cf374f290ea3",
+    "url": "/solid-focus/fonts/roboto-latin-300.55536c8e.woff2"
+  },
+  {
+    "revision": "510dec37fa69fba39593e01a469ee018",
+    "url": "/solid-focus/fonts/roboto-latin-500italic.510dec37.woff2"
+  },
+  {
+    "revision": "ffcc050b2d92d4b14a4fcb527ee0bcc8",
+    "url": "/solid-focus/fonts/roboto-latin-500italic.ffcc050b.woff"
+  },
+  {
+    "revision": "cf6613d1adf490972c557a8e318e0868",
+    "url": "/solid-focus/fonts/roboto-latin-700.cf6613d1.woff"
+  },
+  {
+    "revision": "010c1aeee3c6d1cbb1d5761d80353823",
+    "url": "/solid-focus/fonts/roboto-latin-700italic.010c1aee.woff2"
+  },
+  {
+    "revision": "846d1890aee87fde5d8ced8eba360c3a",
+    "url": "/solid-focus/fonts/roboto-latin-700italic.846d1890.woff"
+  },
+  {
+    "revision": "7b770d6c53423deb1a8e49d3c9175184",
+    "url": "/solid-focus/fonts/roboto-latin-900italic.7b770d6c.woff2"
+  },
+  {
+    "revision": "8c2ade503b34e31430d6c98aa29a52a3",
+    "url": "/solid-focus/fonts/roboto-latin-900.8c2ade50.woff"
+  },
+  {
+    "revision": "bc833e725c137257c2c42a789845d82f",
+    "url": "/solid-focus/fonts/roboto-latin-900italic.bc833e72.woff"
+  },
+  {
+    "revision": "9a6fcaa88384bdb252a649df89b2e6c4",
+    "url": "/solid-focus/index.html"
+  },
+  {
+    "revision": "bca3a1873ac988faff0817eca96b2d86",
+    "url": "/solid-focus/fonts/MaterialIcons-Regular.bca3a187.woff2"
+  },
+  {
+    "revision": "b661c28b0f28606a96722ad2d9588b70",
+    "url": "/solid-focus/fonts/MaterialIcons-Regular.b661c28b.eot"
+  },
+  {
+    "revision": "9219a80f0478e0bfdee5f4c753ce8535",
+    "url": "/solid-focus/fonts/MaterialIcons-Regular.9219a80f.woff"
+  },
+  {
+    "revision": "586090b38a233ce0201fb221eb117a36",
+    "url": "/solid-focus/fonts/MaterialIcons-Regular.586090b3.ttf"
+  },
+  {
+    "revision": "773cb6aca9446d02c7d0",
+    "url": "/solid-focus/css/chunk-vendors.1cd5d9ed.css"
+  },
+  {
+    "revision": "4b4fba4cb9ed9afbcd64",
+    "url": "/solid-focus/css/app.e1fab5ba.css"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,29 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/solid-focus/precache-manifest.f32e0d7fbf7d39f551badb599ae23a5d.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "solid-focus"});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
