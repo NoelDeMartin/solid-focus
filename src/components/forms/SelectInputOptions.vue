421c268a85624d2c409d4c0e663cbe94a1992cbf
Noel De Martin <noel@noeldemartin.com>
2024-06-25 21:34:30 +0200

    Fix workspaces selector

--- src/components/forms/SelectInputOptions.vue
+++ src/components/forms/SelectInputOptions.vue
@@ -1,20 +1,21 @@
 <template>
     <Teleport to="body">
         <ListboxOptions as="template">
-            <ul
+            <component
+                :is="as"
                 v-bind="$attrs"
                 ref="$menu"
                 class="z-50 overflow-auto rounded-lg bg-white text-base shadow-lg ring-1 ring-black/5 focus:outline-none"
             >
                 <slot />
-            </ul>
+            </component>
         </ListboxOptions>
     </Teleport>
 </template>
 
 <script setup lang="ts">
 import { createPopper } from '@popperjs/core';
-import { elementRef, injectReactiveOrFail, mixedProp } from '@aerogel/core';
+import { elementRef, injectReactiveOrFail, mixedProp, stringProp } from '@aerogel/core';
 import { watchEffect } from 'vue';
 import type { Placement } from '@popperjs/core';
 import type { PropType } from 'vue';
@@ -22,7 +23,10 @@ import type { PropType } from 'vue';
 import type { ISelectInput, __SetsSelectElements } from './SelectInput';
 
 defineOptions({ inheritAttrs: false });
-const props = defineProps({ placement: mixedProp<Placement>(String as PropType<Placement>, 'bottom-start') });
+const props = defineProps({
+    as: stringProp('ul'),
+    placement: mixedProp<Placement>(String as PropType<Placement>, 'bottom-start'),
+});
 const $menu = elementRef();
 const input = injectReactiveOrFail<ISelectInput>('input', '<SelectInputButton> must be a child of a <SelectInput>');
 
--- src/pages/workspace/WorkspaceSidebarHeader.vue
+++ src/pages/workspace/WorkspaceSidebarHeader.vue
@@ -9,35 +9,38 @@
                 <span class="max-w-32 text-left text-xl font-semibold">{{ $workspace.name }}</span>
                 <i-zondicons-cheveron-down class="ml-0.5 h-6 w-6" />
             </TextButton>
-            <SelectInputOptions class="flex max-h-60 flex-col overflow-auto pb-10">
-                <SelectInputOption
-                    v-for="workspace in $workspaces.all"
-                    :key="workspace.url"
-                    :value="workspace"
-                    :active-style="{ background: THEME_COLORS[workspace.themeColor][100] }"
-                >
-                    <div
-                        class="mr-2 h-3 w-3 flex-shrink-0 rounded-full"
-                        :style="{ background: THEME_COLORS[workspace.themeColor][500] }"
-                    />
-                    <span :style="{ color: THEME_COLORS[workspace.themeColor][900] }">
-                        {{ workspace.name }}
-                    </span>
-                </SelectInputOption>
-
-                <li class="invisible flex h-0 max-w-[calc(100vw-2rem)] pl-2 pr-3" aria-hidden="true">
-                    <i-material-symbols-add-circle class="h-5 w-5 flex-shrink-0" />
-                    <span class="ml-1">{{ $t('workspaces.add') }}</span>
-                </li>
-
-                <SelectInputOption
-                    :value="ADD_WORKSPACE"
-                    class="absolute inset-x-0 bottom-0 p-0"
-                    inner-class="opacity-1 rounded-none pl-2 pr-3 py-2 hover:bg-gray-100 border-t text-gray-700"
-                >
-                    <i-material-symbols-add-circle class="h-5 w-5 flex-shrink-0" />
-                    <span class="ml-1">{{ $t('workspaces.add') }}</span>
-                </SelectInputOption>
+            <SelectInputOptions as="div" class="pb-10">
+                <ul class="flex max-h-60 flex-col overflow-auto">
+                    <SelectInputOption
+                        v-for="workspace in $workspaces.all"
+                        :key="workspace.url"
+                        :value="workspace"
+                        :active-style="{ background: THEME_COLORS[workspace.themeColor][100] }"
+                    >
+                        <div
+                            class="mr-2 h-3 w-3 flex-shrink-0 rounded-full"
+                            :style="{ background: THEME_COLORS[workspace.themeColor][500] }"
+                        />
+                        <span :style="{ color: THEME_COLORS[workspace.themeColor][900] }">
+                            {{ workspace.name }}
+                        </span>
+                    </SelectInputOption>
+                    <li
+                        class="invisible flex h-0 max-w-[calc(100vw-2rem)] overflow-hidden pl-2 pr-3"
+                        aria-hidden="true"
+                    >
+                        <i-material-symbols-add-circle class="h-5 w-5 flex-shrink-0" />
+                        <span class="ml-1">{{ $t('workspaces.add') }}</span>
+                    </li>
+                    <SelectInputOption
+                        :value="ADD_WORKSPACE"
+                        class="absolute inset-x-0 bottom-0 bg-white p-0"
+                        inner-class="opacity-1 rounded-none pl-2 pr-3 py-2 hover:bg-gray-100 border-t text-gray-700"
+                    >
+                        <i-material-symbols-add-circle class="h-5 w-5 flex-shrink-0" />
+                        <span class="ml-1">{{ $t('workspaces.add') }}</span>
+                    </SelectInputOption>
+                </ul>
             </SelectInputOptions>
         </SelectInput>
         <div class="flex-1" />
