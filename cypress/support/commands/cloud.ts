e48fb44ff0b6c5faf905733c125445d627c5d35b
Noel De Martin <noel@noeldemartin.com>
2025-03-12 13:25:55 +0100

    Fix tests

--- cypress/support/commands/cloud.ts
+++ cypress/support/commands/cloud.ts
@@ -1,6 +1,6 @@
 export function waitSync(): void {
-    cy.see('Syncing');
-    cy.dontSee('Syncing');
+    cy.see('Synchronization in progress');
+    cy.dontSee('Synchronization in progress');
 
     // Wait to make sure there weren't any errors. Otherwise, if this is the last
     // command in a test, the error could be caught after the test has succeeded.
